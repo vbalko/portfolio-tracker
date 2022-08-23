bf0b71514c14486f9345a219f117b990f29a37d9
vbalko <vbalko@gmail.com>
2022-08-23 21:21:24 +0000

    material admin react theme - #1

---
+++ portfolio-tracker/src/config.js
@@ -0,0 +1,8 @@
+const config = {
+  apiKey: "AIzaSyAajCAlsY0YNd41YLHTiDQrTfd6g_IYnDc",
+  authDomain: "portfolio-tracker-9f7ff.firebaseapp.com",
+  databaseURL:
+    "https://portfolio-tracker-9f7ff-default-rtdb.europe-west1.firebasedatabase.app",
+};
+
+export default config;
