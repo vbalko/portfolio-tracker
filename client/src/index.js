a28a7e81a7b6174f1b5961c529f418beb283f3b4
Vlado Balko <vbalko@gmail.com>
2023-01-31 17:21:15 +0100

    added router and context

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,19 @@
 // index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './styles/theme';
 import App from './App';
 
+import { MaterialUIControllerProvider } from './context';
+
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>);
+  <BrowserRouter>
+    <MaterialUIControllerProvider>
+      <App />
+    </MaterialUIControllerProvider>
+  </BrowserRouter>
+  );
